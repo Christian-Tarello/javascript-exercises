12b543310091feb88828885399f8edd611dc93f7
Christian Tarello <christian_tarello@outlook.com>
2022-09-12 20:04:04 -0500

    Mark 02_repeatString as finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function(string, num) {
+    if (num<0){
+        return "ERROR";
+    }
+
     let newString = "";
+
     for (;num>0;num--){
         newString += string;
     }
