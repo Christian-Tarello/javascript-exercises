02d26172e6e6c5b73dfbf00328b32382cf8fc111
Christian Tarello <christian_tarello@outlook.com>
2022-09-12 20:18:03 -0500

    Mark 03_reverseString as finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(content) {
+    let reversedContent = "";
+    for (let i = (content.length-1); i>=0; i--){
+        reversedContent+=content.charAt(i)
+    }
+    return reversedContent
 };
 
 // Do not edit below this line
