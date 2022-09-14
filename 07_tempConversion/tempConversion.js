d5074d678996b567cf70d34ebf0f9587dd160ce4
Christian Tarello <christian_tarello@outlook.com>
2022-09-13 21:30:21 -0500

    Add code to pass all the tests on both functions and mark 07_tempConversion as finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const ftoc = function(degrees) {
-  return (degrees-32)*(5/9);
+  return +(((degrees-32)*(5/9)).toFixed(1));
 };
 
 const ctof = function(degrees) {
-  return (degrees*(5/9))+32
+  return +(((degrees*(9/5))+32).toFixed(1));
 };
 
 // Do not edit below this line
