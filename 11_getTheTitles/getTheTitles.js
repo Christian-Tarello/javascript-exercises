a926f52aa54f3235b336327d20e3c924137a51ac
Christian Tarello <christian_tarello@outlook.com>
2022-09-14 11:34:40 -0500

    Add code to pass all the tests and mark 11_getTheTitles as finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let result = [];
+    array.forEach(function(object){
+        result.push(object.title);
+    });
+    return result;
 };
 
 // Do not edit below this line
