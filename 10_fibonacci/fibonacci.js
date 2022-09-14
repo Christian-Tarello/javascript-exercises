dd0f960bfe10be7c81e6de44ce1cf493403edf3d
Christian Tarello <christian_tarello@outlook.com>
2022-09-14 03:01:45 -0500

    Add code to pass all tests and mark 10_fibonacci as finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num<0) return "OOPS";
+    if (num===0) return num;
+    let last = 1;
+    let secondToLast = 0;
+    let temp;
+    for (let counter = 1; counter<num; counter++){
+        temp = secondToLast;
+        secondToLast = last
+        last = last + temp;
+    }
+    return last;
 };
 
 // Do not edit below this line
