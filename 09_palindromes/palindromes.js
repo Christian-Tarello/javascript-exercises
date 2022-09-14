82b5383d0b43a05293b6d60a25ffa70e500edaf4
Christian Tarello <christian_tarello@outlook.com>
2022-09-14 02:30:45 -0500

    Add code to pass all tests and mark 09_palindromes as finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase()
+    string = string.replace(/[\.!,;-_\[\]\(\)\|\{\}\+\?\*\^\$'"]+|\s+/g, '');
+    let reversedString = string.split('').reverse().join('');
+    return string === reversedString;
 };
 
 // Do not edit below this line
