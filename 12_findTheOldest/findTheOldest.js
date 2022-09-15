9840eee6062155fc47880291202ff63fc0d7c495
Christian Tarello <christian_tarello@outlook.com>
2022-09-14 20:46:54 -0500

    Add code to pass all the tests and mark 12_findTheOldest as finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let oldestAge = 0;
+    let oldestPerson;
+    const today = new Date();
+    const thisYear = today.getFullYear()
+    people.forEach(function(person){
+        const lastYear = person.yearOfDeath || thisYear;
+        const age = lastYear-person.yearOfBirth;
+        if (age>oldestAge){
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
