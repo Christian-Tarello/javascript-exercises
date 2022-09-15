a3b5dacf61a1e0ee75d9d5ce8da7bbfd48ed6cd9
Christian Tarello <christian_tarello@outlook.com>
2022-09-15 07:15:26 -0500

    Add code to pass all the tests and mark 13_caesar as finished

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,56 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    let encodedString = "";
+    for (let i = 0; i<string.length; i++){
+        encodedString += encryptCharacter(string[i], shift);
+    }
+    return encodedString;
 
 };
+
+const encryptCharacter = function(char, shift){
+    const unicode = char.codePointAt(0);
+    const encryptedUnicode = shiftUnicode(unicode, shift);
+    const encryptedChar = String.fromCodePoint(encryptedUnicode);
+    return encryptedChar;
+};
+
+const getUnicodeLimits = function(unicode){
+    const UPPERCASE_LIMITS = [65,90];
+    const LOWERCASE_LIMITS = [97,122];
+
+    switch (true){
+        case unicode>=UPPERCASE_LIMITS[0] && unicode<=UPPERCASE_LIMITS[1]:
+            return UPPERCASE_LIMITS;
+        case unicode>=LOWERCASE_LIMITS[0] && unicode<=LOWERCASE_LIMITS[1]:
+            return LOWERCASE_LIMITS;
+        case true:
+            return undefined;
+    }
+
+};
+
+const wrapNumber = function(num, low, high){
+    /*wrap number inclusively between low & high*/
+    if (num>=low && num<=high) return num;
+    if (num>=low){
+        const range = high-low+1;
+        const wrapped = (num-low)%range;
+        return wrapped+low;
+    } else{
+        const wrapped = low-num;
+        return high-wrapped+1;
+    }
+    
+    
+};
+
+const shiftUnicode = function(unicode, shift){
+    const num = unicode+shift;
+    const unicodeLimits = getUnicodeLimits(unicode);
+    return unicodeLimits ? wrapNumber(num, ...unicodeLimits): unicode;
+};
+
+
 
 // Do not edit below this line
 module.exports = caesar;
